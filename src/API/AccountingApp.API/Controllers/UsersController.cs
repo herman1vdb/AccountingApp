e9f4f071ed29f3b51dbd2fef2711d27153f0499e
Louise van den Berg <louisevdb84@gmail.com>
2018-10-17 15:59:44 +0200

    add get requests for users

---
+++ src/API/AccountingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AccountingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountingApp.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        // GET api/users
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _context.Users.ToList();
+            return Ok(users);
+        }
+
+        // GET api/users/5
+        [HttpGet("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Id == id);
+            return Ok(user);
+        }
+
+        // POST api/users
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/users/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/users/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
