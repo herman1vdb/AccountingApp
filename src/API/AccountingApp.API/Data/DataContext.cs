d1570585bfcc40fda578c454e9d551c56026cb74
Louise van den Berg <louisevdb84@gmail.com>
2018-10-17 15:26:46 +0200

    Add users class and setup entity framework DbContext

---
+++ src/API/AccountingApp.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using AccountingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+        public DbSet<User> Users { get; set; }        
+    }
+}
