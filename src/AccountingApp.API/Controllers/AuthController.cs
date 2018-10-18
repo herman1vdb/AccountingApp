ab64daeca5126d58fccb64119096e40bc2ffebbb
Louise van den Berg <louisevdb84@gmail.com>
2018-10-18 17:17:28 +0200

    Add Dtos and changed register function to use Dto

--- src/AccountingApp.API/Controllers/AuthController.cs
+++ src/AccountingApp.API/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using AccountingApp.API.Data;
+using AccountingApp.API.Dtos;
 using AccountingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,19 +18,19 @@ namespace AccountingApp.API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string username, string password)
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
-            username = username.ToLower();
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-            if (await _repo.UserExist(username))
+            if (await _repo.UserExist(userForRegisterDto.Username))
                 return BadRequest("Username already exists");
 
             var userToCreate = new User
             {
-                Username = username
+                Username = userForRegisterDto.Username
             };
 
-            var createdUser = await _repo.Register(userToCreate, password);
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
         }
