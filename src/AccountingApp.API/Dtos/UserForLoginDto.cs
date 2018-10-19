1e062998d3d23c0c97c3282156e770024f42f978
Louise van den Berg <louisevdb84@gmail.com>
2018-10-19 13:04:18 +0200

    Add login method to authcontroller using JWT

---
+++ src/AccountingApp.API/Dtos/UserForLoginDto.cs
@@ -0,0 +1,12 @@
+namespace AccountingApp.API.Dtos
+{
+    public class UserForLoginDto
+    {        
+        public string Username { get; set; }     
+        public string Password { get; set; }        
+    }
+}
+
+
+
+
