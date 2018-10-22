9c1457d309ebe7e7c2dea490f0638eae22ea24d4
Louise van den Berg <louisevdb84@gmail.com>
2018-10-22 12:18:33 +0200

    clean up home and register component

--- src/AccountingApp-SPA/src/app/home/home.component.ts
+++ src/AccountingApp-SPA/src/app/home/home.component.ts
@@ -9,23 +9,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-  values: any;
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.getValues();
   }
 
   registerToggle() {
     this.registerMode = true;
-  }
-
-  getValues() {
-    this.http.get('http://localhost:5000/api/values').subscribe(response => {
-      this.values = response;
-    }, err => {
-      console.log(err);
-    });
   }
   cancelRegisterMode(registerMode: boolean) {
     this.registerMode = registerMode;
