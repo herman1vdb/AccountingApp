62839e74eb858fbb8315e1e14df9264b859a41d4
Louise van den Berg <louisevdb84@gmail.com>
2019-04-05 14:53:30 +0200

    Add from and to date to display budget

--- src/AccountingApp-SPA/src/app/budget/budget-display/budget-display.component.ts
+++ src/AccountingApp-SPA/src/app/budget/budget-display/budget-display.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Budget } from 'src/app/_models/budget';
 import { BudgetDisplay } from 'src/app/_models/budgetDisplay';
 import { TypeAccount } from 'src/app/_enums/TypeAccount';
-import { DecimalPipe } from '@angular/common';
 import { calcTotalTransPerAccount } from 'src/app/_helpers/calcTotalTrans';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-budget-display',
@@ -13,33 +13,52 @@ import { calcTotalTransPerAccount } from 'src/app/_helpers/calcTotalTrans';
 
 export class BudgetDisplayComponent implements OnInit {
   @Input() budget: Budget[];
+  @ViewChild('creationForm') creationForm: NgForm;
 
   budgetForDisplay: BudgetDisplay[];
   grandTotal: number;
   grandTotalTrans: number;
   grandTotalDiff: number;
   note: string;
+  fromDate: Date;
+  toDate: Date;
 
   constructor() { }
 
   ngOnInit() {
-    const t = 'red';
+    this.budgetForDisplay = [];
+    this.fromDate = null;
+    this.toDate = null;
+    this.setUpBudgetForDisplay();
+  }
+
+  changeDate(event) {
+    if (event.target.id === 'fromDate') {
+      this.fromDate = this.creationForm.value.fromDate;
+    } else
+      if (event.target.id === 'toDate') {
+        this.toDate = this.creationForm.value.toDate;
+      }
+    this.setUpBudgetForDisplay();
+  }
+
+  setUpBudgetForDisplay() {
+    const copiedBudget = JSON.parse(JSON.stringify(this.budget));
     this.budgetForDisplay = [];
     for (let i = 1; i < 5; i++) {
       const budgetDisp: BudgetDisplay = {
-        'budgetList': this.budget.filter(b => b.account.typeId === i && !b.account.isControlAccount),
+        'budgetList': copiedBudget.filter(b => b.account.typeId === i
+          && !b.account.isControlAccount
+          && b.account.isActive
+          && b.account.budget !== 0),
         'typeDescription': TypeAccount[i].toString(),
         'typeId': i,
         'totalBudget': 0,
         'totalTransactions': 0,
         'totalDifference' : 0
       };
-      budgetDisp.budgetList.forEach(b => {
-        b.totalTransactionsPerAcc = calcTotalTransPerAccount(b.transactions, b.account);
-        b.totalDifferencePerAcc = this.calculateDifferencePerAccount(b);
-      });
+      this.budgetListByDate(budgetDisp);
       this.calculateTotals(budgetDisp);
-      this.calculateTotalDifference(budgetDisp);
       this.budgetForDisplay.push(budgetDisp);
     }
     this.calculateGrandTotal();
@@ -47,8 +66,19 @@ export class BudgetDisplayComponent implements OnInit {
     this.calculateGrandTotalDif();
   }
 
+  budgetListByDate(budgetDisp) {
+    console.log(this.fromDate);
+    budgetDisp.budgetList.forEach(b => {
+      if (this.fromDate !== null && this.toDate !== null && this.fromDate.toString() !== '' && this.fromDate.toString() !== '') {
+        b.transactions = b.transactions.filter(t => t.date >= this.fromDate && t.date <= this.toDate);
+      }
+      b.totalTransactionsPerAcc = calcTotalTransPerAccount(b.transactions, b.account);
+      b.totalDifferencePerAcc = this.calculateDifferencePerAccount(b);
+    });
+  }
+
   calculateGrandTotal() {
-    this.grandTotal = this.budgetForDisplay[0].totalBudget
+      this.grandTotal = this.budgetForDisplay[0].totalBudget
       - this.budgetForDisplay[1].totalBudget
       - this.budgetForDisplay[2].totalBudget
       - this.budgetForDisplay[3].totalBudget;
@@ -66,12 +96,13 @@ export class BudgetDisplayComponent implements OnInit {
       - this.budgetForDisplay[3].totalDifference;
   }
   calculateTotals(budgetDisp: BudgetDisplay) {
+    if (budgetDisp.budgetList.length > 0) {
       budgetDisp.totalBudget = budgetDisp.budgetList.map(a => a.account.budget).reduce((sum, current) => sum + current);
       budgetDisp.totalTransactions = budgetDisp.budgetList.map(a => a.totalTransactionsPerAcc).reduce((sum, current) => sum + current);
+      budgetDisp.totalDifference = budgetDisp.totalBudget - budgetDisp.totalTransactions;
+    }
   }
-  calculateTotalDifference(budgetDisp: BudgetDisplay) {
-    budgetDisp.totalDifference = budgetDisp.totalBudget - budgetDisp.totalTransactions;
-  }
+
   calculateDifferencePerAccount(b: Budget) {
     return (b.account.budget - b.totalTransactionsPerAcc);
   }
